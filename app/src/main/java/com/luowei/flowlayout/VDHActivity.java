169715bef851389fa97666dac02e51a9d5fa7632
luowei <wei.luo@karakal.com.cn>
2016-02-18 15:32:22 +0800

    update

---
+++ app/src/main/java/com/luowei/flowlayout/VDHActivity.java
@@ -0,0 +1,17 @@
+package com.luowei.flowlayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 骆巍 on 2016/2/18.
+ */
+public class VDHActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vdh);
+    }
+}
---
+++ app/src/main/java/com/luowei/flowlayout/VDHLayout.java
@@ -0,0 +1,51 @@
+package com.luowei.flowlayout;
+
+import android.content.Context;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by 骆巍 on 2016/2/18.
+ */
+public class VDHLayout extends LinearLayout {
+    private ViewDragHelper mDragger;
+
+    public VDHLayout(Context context) {
+        this(context, null);
+    }
+
+    public VDHLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mDragger = ViewDragHelper.create(this, 1.0f, new ViewDragHelper.Callback() {
+            @Override
+            public boolean tryCaptureView(View child, int pointerId) {
+                return true;
+            }
+
+            @Override
+            public int clampViewPositionHorizontal(View child, int left, int dx) {
+                return left;
+            }
+
+            @Override
+            public int clampViewPositionVertical(View child, int top, int dy) {
+                return top;
+            }
+        });
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return mDragger.shouldInterceptTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mDragger.processTouchEvent(event);
+        return true;
+    }
+}
